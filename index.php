8e9c00396de1f09e4fe6de68dcf7a13021af1498
Lyrna <clarisse.hurion@gmail.com>
2021-01-22 19:46:06 +0100

    carousel avancé

--- index.php
+++ index.php
@@ -6,31 +6,32 @@
   <div class="carousel-inner">
     <!--section première image-->
     <div class="carousel-item active">
-      <img src="assets/img/slider/driver.jpg" class="d-block w-100" alt="First slide">
+      <img src="assets/img/slider/driver.png" class="d-block w-100" alt="First slide">
       <div class="carousel-caption d-none d-lg-block zonetextcarousel">
-        <span class="titretextcarousel">Bienvenue</span>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt uta. Ut enim ad minim veniam,
-        quis nostrud exercitation . </p>
+        <p class="slider-p"><span class="titretextcarousel">Passe ton permis</span><br/>
+          <span class="titretextcarousel">en accéléré</span><br/>
+        </p>
+          <button class="button-slider"><a href="code/permis-accelere.php">J'obtiens mon permis</a></button>
       </div>
     </div>
     <!--section deuxième image-->
     <div class="carousel-item">
-      <img src="assets/img/slider/driver.jpg" class="d-block w-100" alt="Second slide">
+      <img src="assets/img/slider/driver.png" class="d-block w-100" alt="Second slide">
       <div class="carousel-caption d-none d-lg-block zonetextcarousel">
-        <span class="titretextcarousel">Présentation</span>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut  aliqua. Ut eniniam,
-        quis nostrud exercitation ullamco laboris nisi ut. </p>
+        <p class="slider-p"><span class="titretextcarousel">Tu veux apprendre le code</span><br/>
+          <span class="titretextcarousel">dés aujourd'hui ?</span><br/>
+        </p>
+          <button class="button-slider"><a href="code/cours-de-code.php">J'obtiens mon code</a></button>
       </div>
     </div>
         <!--section troisième image-->
     <div class="carousel-item">
-      <img src="assets/img/slider/driver.jpg" class="d-block w-100" alt="Third slide">
+      <img src="assets/img/slider/driver.png" class="d-block w-100" alt="Third slide">
       <div class="carousel-caption d-none d-lg-block zonetextcarousel">
-        <span class="titretextcarousel">Une passion des notes</span>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna . </p>
+        <p class="slider-p"><span class="titretextcarousel">Tu peux voir</span><br/>
+          <span class="titretextcarousel">tous nos permis</span><br/>
+        </p>
+          <button class="button-slider"><a href="code/permis.php">Voir tous les permis</a></button>
       </div>
     </div>   
     </div>
