495b83d41e8f37258f0ce152ad839a86d9e845bd
Lyrna <clarisse.hurion@gmail.com>
2021-01-27 15:58:14 +0100

    padding 80 up pour tous et permis moto avancé

--- code/permis-moto.php
+++ code/permis-moto.php
@@ -5,6 +5,76 @@
   <img src="../assets/img/permis/2.jpg" alt="Permis moto photo" />
 </div>
 
+<!--Section JUMBOTRON-->
+<div class="container-fluid mt-2 mb-5">
+  <div class="landing-page-description">
+    <div class="jumbotron row p-8">
+      <div class="col-12 col-md-8">
+        <h1 class="">Permis moto</h1>
+        <p class="lead">
+          A moto encore plus qu’en voiture le conducteur est vulnérable, c’est pourquoi une formation sérieuse et technique semble être nécessaire à toute personne sensée.
+        </p>
+        <hr class="my-4" />
+        <div class="row">
+          <div>
+            <p>
+              Pour vous accompagner dans votre formation et vous faire progresser sur les différents points techniques importants, l’auto école Montlhéry vous propose une formation assurée par deux moniteurs d’expérience (plus de 700 motards ayant suivit leur formation). 
+            </p>
+          </div>
+          <div id="hiden-description" class="d-none">
+            <p>
+              Vous profiterez d’un matériel adapté qui augmentera toutes vos chances de réussite à l’examen (une moto pour chaque élève : MT-07), un contact radio permanent sur route.Les cours hors circulation s’effectuent sur la piste moto-école qui se trouve à 1,4 km de l’auto-école (à l’entrée de Narbonne coté Coursan proche de la déchetterie) ce qui permet de ne pas perdre de temps pour accéder à celle-ci (4 minutes de trajet).
+            </p>
 
+            <p>
+              La piste moto est partagée avec d’autres moto-école de Narbonne et alentours.
+Le point de rendez-vous est toujours à l’auto-école.
+Les débutants font le trajet de l’auto-école à la piste moto avec la voiture de l’auto-école s’ils sont titulaires du permis B ou éventuellement en passager moto avec l’enseignant.
+            </p>
+          </div>
+
+          <button id="boutton-description-toggle" class="code-interest">
+            Lire plus
+          </button>
+        </div>
+      </div>
+
+      <div class="card-custom-position col-12 col-md-4">
+        <div class="card">
+          <img
+            src="../assets/img/cards/moto.png"
+            alt="icon"
+            class="card-img-top"
+          />
+          <div class="card-body">
+            <p>Passe ton permis moto<br/>
+            avec notre auto-école</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!--Section catégories permis moto-->
+<div class="row">
+  <div class="col-12">
+    <p class="text-center font-weight-bold mb-5 title-section-categories-permis-moto">Il existe depuis le 31 mai 2016 trois catégories de permis moto</p>
+  </div>
+</div>
+<div class="row section-categories-permis-moto p-8">
+  <div class="col-12 col-md-4">
+    <h2 class="text-center font-weight-bold mb-4 nom-permis-moto">A1</h2>
+    <p class="text-center">16 ans</p>
+    <p class="text-center">accès à la conduite des motocyclettes légères limitées à une cylindrée de 125 cm3 et dont la puissance n’excède pas 11 kW (15 CV)</p>
+  </div>
+  <div class="col-12 col-md-4">
+    <h2 class="text-center font-weight-bold mb-4 nom-permis-moto">A2</h2>
+    <p class="text-center">18 ans</p>
+  </div>
+  <div class="col-12 col-md-4">
+    <h2 class="text-center font-weight-bold mb-4 nom-permis-moto">A</h2>
+    <p class="text-center">à partir de 20 ans</p>
+  </div>
+</div>
 <!--footer-->
 <?php include '../includes/footer-page.php'; ?>
