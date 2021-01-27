fdef3b9ba703bed9e473f4a635e0d78638080c5e
Lyrna <clarisse.hurion@gmail.com>
2021-01-27 17:19:22 +0100

    permis moto up

--- code/permis-moto.php
+++ code/permis-moto.php
@@ -64,17 +64,63 @@ Les débutants font le trajet de l’auto-école à la piste moto avec la voitur
 <div class="row section-categories-permis-moto p-8">
   <div class="col-12 col-md-4">
     <h2 class="text-center font-weight-bold mb-4 nom-permis-moto">A1</h2>
-    <p class="text-center">16 ans</p>
+    <p class="text-center age-permis-moto">16 ans</p>
     <p class="text-center">accès à la conduite des motocyclettes légères limitées à une cylindrée de 125 cm3 et dont la puissance n’excède pas 11 kW (15 CV)</p>
   </div>
-  <div class="col-12 col-md-4">
+  <div class="col-12 col-md-4 middle-section-categories-permis-moto">
     <h2 class="text-center font-weight-bold mb-4 nom-permis-moto">A2</h2>
-    <p class="text-center">18 ans</p>
+    <p class="text-center age-permis-moto">18 ans</p>
+    <p class="text-center"> accès à la conduite des motocyclettes dont la puissance est inférieure ou égale à 35 kW (47,6 CV) et d’un rapport puissance / poids inférieur ou égal à 0.2 kW/kg ( 0.27 CV/kg).</p>
+    <p class="description-permis-moto-lower">*Si la moto est bridé, il faut qu’a l’origine sa puissance ne dépasse pas 70kW(95.2CV).</p>
   </div>
   <div class="col-12 col-md-4">
     <h2 class="text-center font-weight-bold mb-4 nom-permis-moto">A</h2>
-    <p class="text-center">à partir de 20 ans</p>
+    <p class="text-center age-permis-moto">à partir de 20 ans</p>
+    <p class="text-center"> 2 ans de permis A2 à l’issue de deux années d’expériences une formation de 7h est necessaire pour valider le permis A.</p>
+    <p class="description-permis-moto-lower">*Vous pouvez effectuer cette formation 3 mois avant le second anniversaire de votre permis A2.</p>
   </div>
+</div>
+<div class="row bell-section-info-permis-moto">
+	<div class="col-12 p-8">
+		<p>
+			<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+  <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
+			</svg>
+			Les titulaires du permis B depuis au moins deux ans peuvent conduire une motocyclette légère sous réserve d’effectuer une formation de 7h donnant lieu à la délivrance d’une attestation de formation permettant de s’assurer et de conduire sur le territoire français uniquement.
+Les personnes ayant obtenu le permis avant le 1 mars 1980 ne sont pas concernés par cette formation ainsi que celles qui étaient déja assurés sur un 125cm3 dans les 5 ans qui précédait le 1 janvier 2011.</p>
+	</div>
+</div>
+<div class="row p-8">
+	<div class="col-12">
+		<h1>Notre programme de formation moto</h1>
+		<h2 class="text-center mt-4 competences-permis-moto">4 compétences générales</h2>
+	</div>
+</div>
+<div class="row p-8 text-center">
+	<div class="col-12 col-md-6 col-xl-3">
+		<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-signpost-2-fill text-center mb-4" viewBox="0 0 16 16">
+  <path d="M7.293.707A1 1 0 0 0 7 1.414V2H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h5v1H2.5a1 1 0 0 0-.8.4L.725 8.7a.5.5 0 0 0 0 .6l.975 1.3a1 1 0 0 0 .8.4H7v5h2v-5h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H9V6h4.5a1 1 0 0 0 .8-.4l.975-1.3a.5.5 0 0 0 0-.6L14.3 2.4a1 1 0 0 0-.8-.4H9v-.586A1 1 0 0 0 7.293.707z"/>
+</svg>
+		<p>Maîtriser le maniement de la moto à vitesse réduite et à vitesse plus élevée hors circulation</p>
+	</div>
+	<div class="col-12 col-md-6 col-xl-3">
+		<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-stoplights-fill text-center mb-4" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M6 0a2 2 0 0 0-2 2H2c.167.5.8 1.6 2 2v2H2c.167.5.8 1.6 2 2v2H2c.167.5.8 1.6 2 2v1a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v-1c1.2-.4 1.833-1.5 2-2h-2V8c1.2-.4 1.833-1.5 2-2h-2V4c1.2-.4 1.833-1.5 2-2h-2a2 2 0 0 0-2-2H6zm3.5 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zM8 13a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+</svg>
+		<p>Appréhender la route et circuler dans des conditions normales</p>
+	</div>
+	<div class="col-12 col-md-6 col-xl-3">
+		<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-droplet-fill text-center mb-4" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 16a6 6 0 0 0 6-6c0-1.655-1.122-2.904-2.432-4.362C10.254 4.176 8.75 2.503 8 0c0 0-6 5.686-6 10a6 6 0 0 0 6 6zM6.646 4.646c-.376.377-1.272 1.489-2.093 3.13l.894.448c.78-1.559 1.616-2.58 1.907-2.87l-.708-.708z"/>
+</svg>
+		<p>Circuler dans des conditions difficiels et partager la route avec les autres usagers</p>
+	</div>
+	<div class="col-12 col-md-6 col-xl-3">
+		<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-brightness-high-fill text-center mb-4" viewBox="0 0 16 16">
+  <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+</svg>
+		<p>Pratiquer une conduite autonome, sûre et économique</p>
+	</div>
 </div>
 <!--footer-->
 <?php include '../includes/footer-page.php'; ?>
