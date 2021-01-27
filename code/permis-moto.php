658f0a66f6bc1d72b12dee4da41ca6aae87101f8
Lyrna <clarisse.hurion@gmail.com>
2021-01-27 14:39:13 +0100

    index push

--- code/permis-moto.php
+++ code/permis-moto.php
@@ -1,5 +1,10 @@
 <!--header-->
 <?php include '../includes/header-page.php'; ?>
 
+<div class="landing-page-image-container">
+  <img src="../assets/img/permis/2.jpg" alt="Permis moto photo" />
+</div>
+
+
 <!--footer-->
 <?php include '../includes/footer-page.php'; ?>
