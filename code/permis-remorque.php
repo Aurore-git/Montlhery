38d40e0d8eec20b9597eaab422d61e941e068b88
Lyrna <clarisse.hurion@gmail.com>
2021-01-28 19:31:40 +0100

    permis remorque fait

--- code/permis-remorque.php
+++ code/permis-remorque.php
@@ -1,5 +1,72 @@
 <!--header-->
 <?php include '../includes/header-page.php'; ?>
 
+<!--Section JUMBOTRON-->
+<div class="container-fluid mt-2">
+  <div class="landing-page-description">
+    <div class="jumbotron row p-8">
+      <div class="col-12 col-md-8">
+        <h1 class="">Permis remorque</h1>
+        <p class="lead">
+          Réforme du 19 janvier 2013 
+        </p>
+        <hr class="my-4" />
+        <div class="row">
+          <div>
+            <p class="font-weight-bold lead">
+              Nous ne dispensons plus les formations aux permis remorque.
+            </p>
+            <p>
+            	Vous pouvez retrouvez ci-dessous les renseignements associés à la formation au permis remorque.
+            </p>
+          </div> 
+        </div>
+      </div>
+
+      <div class="card-custom-position col-12 col-md-4">
+        <div class="card">
+          <img
+            src="../assets/img/cards/moto.png"
+            alt="icon"
+            class="card-img-top"
+          />
+          <div class="card-body">
+            <ul>
+              <li>Passe ton permis moto<br/>
+              avec notre auto-école
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--Section formation B96-->
+<div class="history-permis-moto-grey">
+	<div class="row p-8">
+		<div class="col-12 col-md-6">
+			<h1>Formation B96</h1>
+			<p class="text-center font-weight-bold">Dans quelles conditions le permis B + la formation 7 heures sont-ils nécessaires ?</p>
+			<p>Le permis B et la formation 7 heures sont nécessaires seulement si vous remplissez les 2 conditions suivantes :</p>
+			<ul>
+				<li>Le PTAC (Poids total autorisé en charge) de la remorque est supérieur à 750 kg et inférieur ou égal à 3,5 tonnes</li>
+				<li>La somme des PTAC (PTAC du véhicule tracteur + PTAC de la remorque) est  supérieure à 3.5 tonnes et inférieure ou égale à 4,25 tonnes</li>
+			</ul>
+			<p class="description-permis-moto-lower">Vous trouverez ces informations en regardant votre certificat d’immatriculation au niveau de la mention (F2).</p>
+		</div>
+		<div class="col-12 col-md-6">
+			<h1>Permis E(b)</h1>
+			<p class="text-center font-weight-bold">Dans quelles conditions le permis BE est-il nécessaire ?</p>
+			<p>Le permis BE est nécessaire seulement si vous remplissez les 2 conditions suivantes :</p>
+			<ul>
+				<li>Le PTAC (Poids total autorisé en charge) de la remorque est supérieur à 750 kg et inférieur ou égal à 3,5 tonnes</li>
+				<li>La  somme des PTAC du véhicule tracteur et de la remorque est supérieure à 4,25 tonnes</li>
+			</ul>
+		</div>
+	</div>
+</div>
+<!--Section formation permis-->
 <!--footer-->
 <?php include '../includes/footer-page.php'; ?>
