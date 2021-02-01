2e4fd5b266f1412df24cef433b93c25b0c0db7fa
Lyrna <clarisse.hurion@gmail.com>
2021-02-01 17:21:46 +0100

    permis auto en cours

--- code/permis-auto.php
+++ code/permis-auto.php
@@ -1,5 +1,147 @@
 <!--header-->
 <?php include '../includes/header-page.php'; ?>
 
+<div class="landing-page-image-container">
+  <img src="../assets/img/permis/car.jpg" alt="femme au volant" />
+</div>
+
+<!--Section JUMBOTRON-->
+<div class="container-fluid mt-2 mb-5">
+  <div class="landing-page-description">
+    <div class="jumbotron row p-8">
+      <div class="col-12 col-md-8">
+        <h1 class="">Permis B Voiture</h1>
+          <p class="lead">
+              Pour passer le permis B, il faut avoir obtenu son code, mais il est possible de préparer les deux simultanément.<br/>
+              L’auto-école Montlhéry met à votre disposition 7 enseignants de la conduite automobile diplômés et expérimentés
+              et 7 véhicules climatisés (des nouvelles Clio) dont 2 avec boite à vitesse automatique.<br/>
+              Cette grande plage horaire offre de grandes possibilités de rendez vous en fonction de votre emploi du temps.
+          </p>
+
+            <hr class="my-4" />
+
+        <div class="row">
+            <p>
+            Il est donc possible de concilier la préparation du permis de conduire et la vie professionnelle ou les études.
+            De plus l’auto école étant située au centre ville proche des lycées Beauséjour, Diderot et Dr Lacroix 
+            (rendu facile d’accès par au sens unique), nous pouvons venir chercher l’élève sans perte de temps au lycée 
+            ou même à domicile.
+            </p>
+        </div>
+
+        <div id="situation" class="collapse">
+        	<p>
+        		Les cours de conduites ont lieu :
+        		<ul>
+        			<li>du lundi au vendredi de 8h à 12h et de 13h à 19h</li>
+        			<li>le samedi de 8h à 12h et de 13h à 18h</li>
+        		</ul>
+        	</p>
+            <p>
+            La situation géographique de l’auto-école a aussi des avantages pédagogiques étant placée proche du centre d’examen, 
+            de quartiers calmes, de la rocade, routes départementales ou encore épargnée par les bouchons des heures de pointes 
+            pour que chaque leçon soit le plus profitable possible.
+            </p>
+            <p>
+            A ces délais se rajoute le problème de disponibilités des places
+            d’examen (qu’il faut prévoir un petit peu à l’avance) que ce soit
+            pratique ou théorique.
+            </p>
+        </div>
+        <button type="button" data-target="#situation" data-toggle="collapse" 
+        aria-expanded="false" aria-controls="situation" class="code-interest">Lire la suite</button>
+      </div>
+
+      <div class="card-custom-position col-12 col-md-4">
+        <div class="card">
+          <img src="../assets/img/cards/voiture.png"  width="50%" height="50%"
+          alt="icon" class="card-img-top"/>
+            <div class="card-body">
+              <p>Les cours de conduites ont lieu :<br/><br/>
+                  Du lundi au vendredi:<br/> de 8h à 12h et de 13h à 19h<br/><br/>
+                  Le samedi:<br/> de 8h à 12h et de 13h à 18h<br/>
+              </p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--Section Enjeux-->
+<div class="container-fluid section-exam my-5">
+  <div class="custom-row-exam row">
+    <div class="col-11 col-lg-7 description-code">
+      <h1 class="left">Enjeux de la formation</h1>
+      <p>
+        L’automobile est devenue un outil social indispensable pour une très grande partie des jeunes 
+        et moins jeunes de notre société.
+        Au-delà du plaisir de conduire, l’utilisation d’une voiture est souvent indispensable pour les études, 
+        le travail ou les loisirs.<br/> Rouler en sécurité est donc une nécessité pour tous.
+        <br>Les conducteurs débutants représentent une part trop importante des tués et des blessés sur la route.
+        L’effort engagé doit être poursuivi, notamment par le renforcement de l’éducation et de la formation.
+      </p>
+      <p>
+        Les accidents de la route ne sont pas liés à la fatalité et pour aider les nouveaux conducteurs à se 
+        déplacer avec un risque faible de perdre la vie ou de la dégrader un programme de formation est mis en 
+        place dans votre auto-école.<br/>
+        L’objectif général est d’amener tout automobiliste débutant à la maitrise de compétences en termes de savoir être, 
+        savoirs, savoir- faire et savoir devenir.
+        Apprendre à conduire est une démarche éducative exigeante, notre équipe d’enseignants expérimentée et diplômée de 
+        la sécurité routière vous guidera dans votre progression.
+      </p>
+    </div>
+  </div>
+</div>
+
+<!-- Section formation -->
+<div class="container-fluid section-formule my-5">
+  <h1 class="mb-4">Déroulement de la formation</h1>
+  <div class="row justify-content-around">
+    <!-- <div class="col-2 fomule-images d-flex flex-column">
+      <img src="../assets/img/cards/formule.png.svg" alt="image formule" />
+    </div> -->
+    <p class="col-11 formule-description">
+        Pour la formation au code de la route, vous aurez <strong>plusieurs outils</strong> à votre disposition.<br /><br />
+      <strong>- Un livre de code</strong><br/>
+      <strong>- Un cours hebdomadaire</strong> sur la règlementation dispensé par un enseignant qui reprend une à une les règles 
+        du code de la route.<br/>
+      <strong>- Deux examens blancs par jour</strong> corrigés et expliqués par un enseignant qui développe un cours interactif 
+        sur le thème de la question abordée.<br/>
+      <strong>- 5 heures par jour</strong> de questionnaires corrigés.<br/>
+      <strong>- Un accés personnalisé</strong> à notre plateforme d’exercice sur internet qui fonctionne sur ordinateur 
+      , mobile ou   tablette comme à l’examen.<br/>
+    </p>
+  </div>
+</div>
+
+<!-- Section pratique -->
+<div class="section-formation">
+  <div class="row p-8">
+    <div class="col-12 col-lg-9 ">
+      <h1 class="text-left">
+        Formation pratique
+      </h1>
+      <p class="text-justify lead">
+        Pour la formation pratique, une progression propre à notre auto-école s’adaptera à vos particularités. 
+      </p>
+      <hr/>
+      <p>
+        Nous débuterons tout d’abord sur un parking où vous apprendrez le maniement 
+        du véhicule (démarrer, s’arrêter, passer les vitesses, tourner le volant…) puis nous continuerons sur la route.
+      </p>
+      <div id="formation">
+        <p>Progressivement nous entrerons en circulation et vous développerez des compétences nécessaire à 
+        une conduite sure qui vous emmènera progressivement au niveau requis pour l’examen.<br/>
+        Vous aurez la possibilité de conduire sur véhicule à boite de vitesse manuelle ou automatique, d’adapter 
+        la fréquence de vos rendez-vous avec une équipe de 7 enseignants qui collaborent pour votre réussite.
+        Les formations théoriques et pratiques peuvent être réalisées simultanément mais nous recommandons 
+        d’avoir acquis suffisamment de connaissances théoriques avant de commencer la pratique.</p>
+      </div>
+      <button type="button" data-target="#formation" data-toggle="collapse" aria-expanded="false" 
+      aria-controls="formation" class="code-interest">Lire la suite</button>
+    </div>
+  </div>
+</div>
 <!--footer-->
 <?php include '../includes/footer-page.php'; ?>
